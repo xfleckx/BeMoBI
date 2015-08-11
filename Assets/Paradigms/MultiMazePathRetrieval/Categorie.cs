c3298ad7d4b74663072a5c2b3060265c9d454412
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-08-12 00:41:12 +0200

    wip: Object and Categorie representation

---
+++ Assets/Paradigms/MultiMazePathRetrieval/Categorie.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Category : MonoBehaviour
+{
+    public GameObject GetObjectByID(string name)
+    {
+        int childCount = transform.childCount;
+
+        if (childCount == 0)
+            throw new MissingComponentException("Categorie has no Objects");
+
+        for (int i = 0; i < childCount; i++)
+        {
+            var childTransform = transform.GetChild(i);
+
+            if (childTransform.name.Equals(name))
+                return childTransform.gameObject;
+        }
+
+        throw new ArgumentException(string.Format("Object \"{0}\" not found!", name));
+    }
+}
---
+++ Assets/Paradigms/MultiMazePathRetrieval/ObjectManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class ObjectManager : MonoBehaviour, IEnumerable<Category>
+{
+    public List<Category> Categories;
+    
+    public Category GetCategoryBy(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            throw new ArgumentException("empty category name");
+
+        var category = Categories.SingleOrDefault((c) => c.name.Equals(name));
+
+        if (category == null)
+            throw new ArgumentException(string.Format("Requested Category \"{0}\" not found", name));
+
+        return category;
+    }
+
+    public IEnumerator<Category> GetEnumerator()
+    {
+        return new CategoryEnumerator(Categories);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class CategoryEnumerator : IEnumerator<Category>
+{
+    private Stack<Category> stack;
+
+    public CategoryEnumerator(List<Category> categories)
+    {
+        stack = new Stack<Category>(categories);
+    }
+
+    public Category Current
+    {
+        get
+        {
+            return stack.Peek();
+        }
+    }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return stack.Peek();
+        }
+    }
+
+    public void Dispose()
+    {
+        stack.Clear();
+    }
+
+    public bool MoveNext()
+    {
+        if (stack.Count > 0)
+        {
+            stack.Pop();
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Reset()
+    {
+        stack = new Stack<Category>(stack);
+    }
+}
