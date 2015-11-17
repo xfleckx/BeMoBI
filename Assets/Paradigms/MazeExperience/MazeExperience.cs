ec60c3f224db75dca8f06227d3c985d0d796dba8
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-11-17 14:04:35 +0100

    [REFAC] Remove deprecated code

--- Assets/Paradigms/MazeExperience/MazeExperience.cs
+++ Assets/Paradigms/MazeExperience/MazeExperience.cs
@@ -24,7 +24,6 @@ public class MazeExperience : MonoBehaviour
     
     void Start()
     {
-        subject.SetMultipleInputMethods(subject.RiftOnlyInput, subject.XBoxActions);
 
         startPoint.EnterStartPoint += subjectEntersStartPoint; 
 
