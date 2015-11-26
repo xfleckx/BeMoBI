8640f85278c9c0d5798115715929e957688955bf
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-11-26 12:57:11 +0100

    [FEATURE] Add data export to labstreaminglayer subscriber

---
+++ Assets/Paradigms/MultiMazePathRetrieval/Scripts/LSLBodyOrientation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using LSL;
+
+public class LSLBodyOrientation : MonoBehaviour {
+
+
+    private liblsl.StreamOutlet outlet;
+    private liblsl.StreamInfo streamInfo;
+    private float[] currentSample;
+
+    public string StreamName = "BeMoBI.Unity.BodyOrientation";
+    public string StreamType = "Unity.Quarternion";
+    public int ChannelCount = 4;
+
+    public Transform body;
+
+    void Start()
+    {
+        currentSample = new float[ChannelCount];
+
+        streamInfo = new liblsl.StreamInfo(StreamName, StreamType, ChannelCount, Time.fixedDeltaTime);
+
+        outlet = new liblsl.StreamOutlet(streamInfo);
+    }
+
+    public void FixedUpdate()
+    {
+        var rotation = body.rotation;
+
+        currentSample[0] = rotation.x;
+        currentSample[1] = rotation.y;
+        currentSample[2] = rotation.z;
+        currentSample[3] = rotation.w;
+        outlet.push_sample(currentSample);
+    }
+}
---
+++ Assets/Paradigms/MultiMazePathRetrieval/Scripts/LSLHeadOrientation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using LSL;
+
+public class LSLHeadOrientation : MonoBehaviour {
+
+    private liblsl.StreamOutlet outlet;
+    private liblsl.StreamInfo streamInfo;
+    private float[] currentSample;
+
+    public string StreamName = "BeMoBI.Unity.HeadOrientation";
+    public string StreamType = "Unity.Quaternion";
+    public int ChannelCount = 4;
+
+    public Transform head;
+
+    void Start()
+    {
+        currentSample = new float[ChannelCount];
+
+        streamInfo = new liblsl.StreamInfo(StreamName, StreamType, ChannelCount, Time.fixedDeltaTime);
+
+        outlet = new liblsl.StreamOutlet(streamInfo);
+    }
+
+    public void FixedUpdate()
+    {
+        var rotation = head.rotation;
+
+        currentSample[0] = rotation.x;
+        currentSample[1] = rotation.y;
+        currentSample[2] = rotation.z;
+        currentSample[3] = rotation.w;
+        outlet.push_sample(currentSample);
+    }
+}
