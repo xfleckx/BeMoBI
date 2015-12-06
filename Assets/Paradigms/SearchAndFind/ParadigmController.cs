6099cab56c30f5c483f06bed4d41a69f0aec2e13
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-12-06 02:38:16 +0100

    [WIP] Rewrite Markerpattern

--- Assets/Paradigms/SearchAndFind/ParadigmController.cs
+++ Assets/Paradigms/SearchAndFind/ParadigmController.cs
@@ -297,15 +297,61 @@ public class ParadigmController : MonoBehaviour
 
 public static class MarkerPattern
 {
-    public const string BeginTrial = "{0}_{1}_{2}_{3}_BeginTrial";
-    public const string Unit = "{0}_Unit_{1}_{2}";
-    public const string Enter = "Entering_{0}_{1}_{2}";
-    public const string Turn = "{0}_Turn";
+    public const string BeginTrial = "BeginTrial#{0}#{1}#{2}#{3}#{4}";
 
-    public const string L = "L";
-    public const string R = "R";
-    public const string Correct = "Correct"; // TODO: integrate into other marker 
-    public const string Incorrect = "Incorrect";
+    public const string Unit = "{0}#Unit#{1}#{2}";
+
+    public const string Enter = "Entering#{0}#{1}#{2}";
+
+    public const string ObjectFound = "ObjectFound#{0}#{1}#{2}#{3}";
+    
+    // TODO: Grid ID's should not be renderered as float values
+    /// <summary>
+    /// From {0} to {1} TurnType {2} UnitType {3}
+    /// </summary>
+    public const string Turn = "Turn#From:{0}#To:{1}#{2}#{3}";
+
+    /// <summary>
+    /// From {0} to {1} expected {2} TurnType {3} UnitType {4}
+    /// </summary>
+    public const string WrongTurn = "Incorrect#From:{0}#To:{1}#Exp:{2}#{3}#{4}";
+
+    public const string EndTrial = "EndTrial#{0}#{1}#{2}#{3}#{4}";
+
+    public static string FormatBeginTrial(string trialTypeName, string mazeName, int pathId, string objectName, string categoryName)
+    {
+        return string.Format(BeginTrial, trialTypeName, mazeName, pathId, objectName, categoryName);
+    }
+
+    public static string FormatCorrectTurn(PathElement lastPathElement, PathElement currentPathElement)
+    {
+        var lastGridId = lastPathElement.Unit.GridID;
+
+        var currentGridId = currentPathElement.Unit.GridID;
+
+        return string.Format(Turn, lastGridId, currentGridId, lastPathElement.Type, lastPathElement.Turn);
+    }
+
+    public static string FormatIncorrectTurn(MazeUnit wrongUnitEntered, PathElement lastPathElement, PathElement expectedUnit)
+    {
+        var wrongGridId = wrongUnitEntered.GridID;
+
+        var lastGridId = lastPathElement.Unit.GridID;
+
+        var expectedGridId = expectedUnit.Unit.GridID;
+
+        return string.Format(WrongTurn, lastGridId, wrongGridId, expectedGridId, lastPathElement.Type, lastPathElement.Turn);
+    }
+
+    public static string FormatFoundObject(string currentMazeName, int iD, string objectName, string categoryName)
+    {
+        return string.Format(ObjectFound, currentMazeName, iD, objectName, categoryName);
+    }
+
+    public static string FormatEndTrial(string trialTypeName, string mazeName, int pathId, string objectName, string categoryName)
+    {
+        return string.Format(EndTrial, trialTypeName, mazeName, pathId, objectName, categoryName);
+    }
 }
 
 #region TODO: Save instance state (SaveGames)
