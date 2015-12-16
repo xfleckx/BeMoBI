ab1ef0621c9428d74f8e39ce32583e7bca35e292
Markus <fleckmarkus.privat@googlemail.com>
2015-12-16 17:25:33 +0100

    [FEATURES] Add new Assets to provide a new font, a new model for the world and a rotation animation

---
+++ Assets/Paradigms/SearchAndFind/ParadigmInstanceDefinition.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParadigmInstanceDefinition : ScriptableObject
+{
+    public string Subject;
+    public string BodyController;
+    public string HeadController;
+
+    [SerializeField]
+    public List<TrialDefinition> Trials;
+
+}
