7fea188bfe3672c6c398770057c0b17824b87ea0
Markus Fleck <fleckmarkus.privat@gmail.com>
2015-12-18 03:45:17 +0100

    [WIP FEATURE] Add Teleportation script

---
+++ Assets/Paradigms/SearchAndFind/Scripts/Teleporting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Teleporting : MonoBehaviour {
+
+    public GameObject ObjectToTeleport;
+
+    public Transform Target;
+
+    public UnityEvent BeforeTeleporting;
+
+    public UnityEvent AfterTeleporting;
+    
+    public void Teleport()
+    {
+        if (BeforeTeleporting != null)
+            BeforeTeleporting.Invoke();
+
+        ObjectToTeleport.transform.position = Target.position;
+
+        if (AfterTeleporting != null)
+            AfterTeleporting.Invoke();
+    }
+}
