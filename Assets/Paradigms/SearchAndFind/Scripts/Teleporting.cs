c05c143dc9ae3722f0bf9a40c481937a60017069
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-01-10 15:55:27 +0100

    [FEATURE] Update Teleporting against the more flexible Screen fading script

--- Assets/Paradigms/SearchAndFind/Scripts/Teleporting.cs
+++ Assets/Paradigms/SearchAndFind/Scripts/Teleporting.cs
@@ -1,25 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Events;
+using System;
 
 public class Teleporting : MonoBehaviour {
+    
+    public float ExpectedDuration = 2f;
 
     public GameObject ObjectToTeleport;
 
     public Transform Target;
 
-    public UnityEvent BeforeTeleporting;
+    public Transform postTeleportLookAt;
 
-    public UnityEvent AfterTeleporting;
+    public TeleportingDurationEvent BeforeTeleporting;
+
+    public TeleportingDurationEvent AfterTeleporting;
     
     public void Teleport()
     {
         if (BeforeTeleporting != null)
-            BeforeTeleporting.Invoke();
+            BeforeTeleporting.Invoke(ExpectedDuration);
 
+        StartCoroutine(TeleportingProcess()); 
+    }
+
+    IEnumerator TeleportingProcess()
+    {
+        yield return new WaitForSeconds(ExpectedDuration);
+        
         ObjectToTeleport.transform.position = Target.position;
 
+        ObjectToTeleport.transform.LookAt( postTeleportLookAt );
+        
         if (AfterTeleporting != null)
-            AfterTeleporting.Invoke();
+            AfterTeleporting.Invoke(ExpectedDuration);
+
+        yield return null;
     }
+}
+
+[Serializable]
+public class TeleportingDurationEvent : UnityEvent<float>
+{
+    // does nothing
 }
