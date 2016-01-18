11b951c4b15756db99f1cb6eb107fa958ef35743
Markus <fleckmarkus.privat@googlemail.com>
2016-01-18 14:37:08 +0100

    [REFAC] Paradigm Config to dedicated file

--- Assets/Paradigms/SearchAndFind/ParadigmController.cs
+++ Assets/Paradigms/SearchAndFind/ParadigmController.cs
@@ -650,47 +650,4 @@ namespace Assets.Paradigms.SearchAndFind
         }
     }
 
-    [Serializable]
-    public class ParadigmConfiguration : ScriptableObject
-    {
-        [SerializeField]
-        public bool useTeleportation = false;
-
-        [SerializeField]
-        public bool writeStatistics = false;
-
-        [SerializeField]
-        public bool ifNoInstanceDefinitionCreateOne = false;
-
-        [SerializeField]
-        public float TimeToDisplayObjectToRememberInSeconds = 3;
-        
-        [SerializeField]
-        public float TimeToDisplayObjectWhenFoundInSeconds = 2;
-
-        [SerializeField]
-        public float offsetToTeleportation = 2;
-
-        [SerializeField]
-        public int categoriesPerMaze = 1;
-
-        [SerializeField]
-        public int mazesToUse;
-
-        [SerializeField]
-        public int pathsToUsePerMaze; // corresponds with the available objects - one distinct object per path per maze
-
-        [SerializeField]
-        public int objectVisitationsInTraining = 1; // how often an object should be visisted while trainings trial
-
-        [SerializeField]
-        public int objectVisitationsInExperiment = 1; // " while Experiment
-
-        [SerializeField]
-        public bool useExactOnCategoryPerMaze = true;
-
-        [SerializeField]
-        public bool groupByMazes = true;
-        
-    }
 }
