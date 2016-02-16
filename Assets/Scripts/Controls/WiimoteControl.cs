a912cf5068b43e8c76c84e4eed894bb7a2a65135
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-02-16 19:05:44 +0100

    Add Wiimote support

---
+++ Assets/Scripts/Controls/WiimoteControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using WiimoteApi;
+
+public class WiimoteControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
