41c7fc25a635cba0dc1ce0476e2d8d267a8afcb2
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-06-08 12:37:30 +0200

    [REFAC] Change variable to paradigm instead of controller to improve naming consistency

--- Assets/Paradigms/SearchAndFind/Scripts/SubjectInteractions.cs
+++ Assets/Paradigms/SearchAndFind/Scripts/SubjectInteractions.cs
@@ -11,29 +11,24 @@ namespace Assets.BeMoBI.Paradigms.SearchAndFind {
 		private const string SUBMIT_INPUT = "Subject_Submit";
 		private const string REQUIRE_PAUSE = "Subject_Requires_Break";
 
-		ParadigmController controller;
-		
-		public bool TestMode = false;
+		ParadigmController paradigm;
 
-		private bool homeButtonIsDown = false;
-		
 		void Awake()
 		{
-			controller = FindObjectOfType<ParadigmController>();
+			paradigm = FindObjectOfType<ParadigmController>();
 		}
 
 		// Update is called once per frame
 		void Update () {
 			
-
 			if (Input.GetAxis(SUBMIT_INPUT) > 0)
 			{
-				controller.SubjectTriesToSubmit();
+				paradigm.SubjectTriesToSubmit();
 			}
 
 			if (Input.GetAxis(REQUIRE_PAUSE) > 0)
 			{
-				controller.ForceABreakInstantly();
+				paradigm.ForceABreakInstantly();
 			}
 		}
 	}
