5e4484167735f77ef958cabbf30a8b9299909376
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-11-27 20:14:43 +0100

    Add documentation for FreeFlyCamera

--- Assets/PhaseSpaceTesting/FreeFlyRiftPSRigid.cs
+++ Assets/PhaseSpaceTesting/FreeFlyRiftPSRigid.cs
@@ -1,22 +1,35 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿
+using UnityEngine.Assertions;
 
-public class FreeFlyRiftPSRigid : OWLRigidController
+/// <summary>
+/// This an instance of this class
+/// </summary>
+public class FreeFlyRiftPSRigid : OWLCustomLRigidController
 {
+    public OVRCameraRig targetCamRig;
+
     public override void Start()
     {
-        OVRCameraRig rig = FindObjectOfType<OVRCameraRig>();
+        if(targetCamRig == null)
+            targetCamRig = FindObjectOfType<OVRCameraRig>();
 
-        rig.UpdatedAnchors += Rig_UpdatedAnchors;
+        Assert.IsNotNull(targetCamRig, "There is no OVRCameraRig Instance in the Scene.");
+        
+        targetCamRig.UpdatedAnchors += Rig_UpdatedAnchors;
     }
 
-    private void Rig_UpdatedAnchors(OVRCameraRig obj)
+    /// <summary>
+    /// This got called after OVR Anchors got all updates from the headset sensors.
+    /// </summary>
+    /// <param name="rig"></param>
+    private void Rig_UpdatedAnchors(OVRCameraRig rig)
     {
-        obj.transform.position = prevPos;
-
+        rig.transform.position = prevPos;
     }
 
-    //
+    /// <summary>
+    /// Message from Unity got called before the camera gets rendered.
+    /// </summary>
     void OnPreRender()
     {
         // call right before render as well, to ensure most recent data.
